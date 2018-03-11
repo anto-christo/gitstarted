24eaabdb6d8ec97cee040e38f70458f406b801cb
Anto <anto.christo.20@gmail.com>
2018-03-11 22:38:38 +0530

    Remove unwanted file. Add screenshot of search bar

--- test.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-		type:'GET',
-		url:'/https://api.github.com/search/repositories?q=language:html&sort=stars&order=desc',
-		data:{token:token},
-		dataType:'json',
-		success: function(data){
-			console.log(data)
-		}
-	});
